9dbb6f8295e8b1400e332bfba8dcf465f863f55e
MohamedMohamed <m01126165016@icloud.com>
2024-07-07 04:27:01 +0300

    first commit

---
+++ Variable_Datatypes.js
@@ -0,0 +1,24 @@
+var x
+console.log(x);
+console.log(typeof x);
+console.log("------------------------------------------");
+
+x = 10
+console.log(x);
+console.log(typeof x);
+console.log("------------------------------------------");
+
+x = "Hello world"
+console.log(x);
+console.log(typeof x);
+console.log("------------------------------------------");
+
+x = true
+console.log(x);
+console.log(typeof x);
+console.log("------------------------------------------");
+
+x = null
+console.log(x);
+console.log(typeof x);
+console.log("------------------------------------------");
