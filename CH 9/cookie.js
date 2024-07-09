576e37ce088893520f545501aaec8a8db32c8349
MohamedMohamed <m01126165016@icloud.com>
2024-07-10 02:05:44 +0300

    cookies

---
+++ CH 9/cookie.js
@@ -0,0 +1,33 @@
+function saveInfo() {
+    //Persistent Cookie
+    var date = new Date();
+    date.setMonth(date.getMonth() + 2);
+    var valName = document.getElementById("name").value;
+    var valEmail = document.getElementById("email").value;
+    var valPassword = document.getElementById("password").value;
+    document.cookie = "UserName=" + valName + ";expires=" + date;
+    document.cookie = "UserEmail=" + valEmail + ";expires=" + date;
+    document.cookie = "UserPassword=" + valPassword + ";expires=" + date;
+}
+
+function getAllCookies() {
+    var splitCookie = document.cookie.split(";");
+    var assoiativeCookie = [];
+    for (i = 0; i < splitCookie.length; i++) {
+        assoiativeCookie[splitCookie[i].split("=")[0].trim()] =
+            splitCookie[i].split("=")[1];
+    }
+    return assoiativeCookie;
+}
+
+function displayGreeting() {
+    var cookie = getAllCookies();
+    var name = cookie["UserName"];
+    var email = cookie["UserEmail"];
+    var h1 = document.createElement("h1");
+    document.body.appendChild(h1);
+
+    var text = "Hello, " + name + " !" + "\n Your Email is " + email;
+
+    h1.appendChild(document.createTextNode(text))
+}
