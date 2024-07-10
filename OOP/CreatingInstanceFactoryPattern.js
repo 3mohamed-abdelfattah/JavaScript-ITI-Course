1a5fa6487b60924909cff69e40827928d49ac89b
MohamedMohamed <m01126165016@icloud.com>
2024-07-10 13:48:04 +0300

    OOP

---
+++ OOP/CreatingInstanceFactoryPattern.js
@@ -0,0 +1,9 @@
+var fun = function (name, age) {
+    return {
+        name: name,
+        age: age
+    }
+}
+
+
+console.log(fun("Mohamed", "2"))
---
+++ OOP/Instance.js
@@ -0,0 +1,12 @@
+var Employee = function (sal, dept) {
+
+    this.sal = sal;
+    this.dept = dept;
+}
+
+var emp1 = new Employee(20000, "Software Engineer")
+console.log(emp1)
+
+var emp2 = new Employee(20000, "Engineer")
+console.log(emp2)
+console.log(emp1)
